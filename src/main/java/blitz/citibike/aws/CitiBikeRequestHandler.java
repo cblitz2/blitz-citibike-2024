a5c9dd49161392e640e201a0400f477da8f27b67
FIRST_NAME LAST_NAME <cblitz2@student.touro.edu>
2025-01-01 10:48:17 -0800

    fixed comments

--- src/main/java/blitz/citibike/aws/CitiBikeRequestHandler.java
+++ src/main/java/blitz/citibike/aws/CitiBikeRequestHandler.java
@@ -8,18 +8,17 @@ import com.amazonaws.services.lambda.runtime.Context;
 
 public class CitiBikeRequestHandler implements RequestHandler
         <APIGatewayProxyRequestEvent, CitiBikeResponse> {
-    private final FindStation findStation;
-
-    public CitiBikeRequestHandler() {
-        CitiBikeService service = new CitiBikeServiceFactory().getService();
-        this.findStation = new FindStation(service);
-    }
 
     @Override
     public CitiBikeResponse handleRequest(APIGatewayProxyRequestEvent event, Context context) {
         String body = event.getBody();
         Gson gson = new Gson();
         CitiBikeRequest request = gson.fromJson(body, CitiBikeRequest.class);
+
+        CitiBikeServiceFactory serviceFactory = new CitiBikeServiceFactory();
+        CitiBikeService service = serviceFactory.getService();
+
+        FindStation findStation = new FindStation(service);
 
         StationsResponse.Station startStation = findStation.findClosestAvailable(request.from.lat, request.from.lon);
         StationsResponse.Station endStation = findStation.findClosestStation(request.to.lat, request.to.lon, false);
