3d6ff6f10fc4e5c01a85867bc4c60a8f5c306590
FIRST_NAME LAST_NAME <cblitz2@student.touro.edu>
2025-01-02 18:47:02 -0800

    fixed comments

--- src/main/java/blitz/citibike/aws/CitiBikeRequestHandler.java
+++ src/main/java/blitz/citibike/aws/CitiBikeRequestHandler.java
@@ -18,10 +18,15 @@ public class CitiBikeRequestHandler implements RequestHandler
         CitiBikeServiceFactory serviceFactory = new CitiBikeServiceFactory();
         CitiBikeService service = serviceFactory.getService();
 
+        StationsResponse stationsResponse = service.getStationInformation().blockingGet();
+        StatusResponse statusResponse = service.getStationStatus().blockingGet();
+
         FindStation findStation = new FindStation(service);
 
-        StationsResponse.Station startStation = findStation.findClosestAvailable(request.from.lat, request.from.lon);
-        StationsResponse.Station endStation = findStation.findClosestStation(request.to.lat, request.to.lon, false);
+        StationsResponse.Station startStation = findStation.findClosestAvailable(
+                stationsResponse, statusResponse, request.from.lat, request.from.lon);
+        StationsResponse.Station endStation = findStation.findClosestReturn(
+                stationsResponse, statusResponse, request.to.lat, request.to.lon);
 
         return new CitiBikeResponse(request.from, request.to, startStation, endStation);
     }
